c622b295264a2d3709e280b2384530aced4ed96d
sora <vaz1306011@gmail.com>
2025-09-27 10:20:29 +0900

    fix vscode ctrl+/ keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+local vscode = require('vscode')
+
+if vim.g.vscode then
+  vim.keymap.set({ "n", "x", "o" }, "<C-/>", function()
+      vscode.action("editor.action.commentLine")
+  end)
+end
