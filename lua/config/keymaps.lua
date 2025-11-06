9059e93d559122a3d46f41fc8cd4c8e368d7ddc2
sora <vaz1306011@gmail.com>
2025-11-06 20:06:25 +0900

    新增 行移動快捷鍵

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,14 @@ if vim.g.vscode then
       vscode.action("editor.action.commentLine")
   end)
 end
+-- Normal 模式：移動當前行
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Visual 模式：移動選取範圍
+vim.keymap.set("x", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("x", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Insert 模式（可選）：按住 Alt 也能移動整行
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
