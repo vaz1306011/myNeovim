967dcf2bdadf436f1620fbd01b0a4c7bbf2ca507
sora <vaz1306011@gmail.com>
2026-05-13 09:59:03 +0900

    關閉 vscode-neovim message

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.g.vscode then
+  vim.opt.cmdheight = 1
+
+  vim.opt.shortmess:append({
+    s = true,
+    I = true,
+    W = true,
+    c = true,
+    C = true,
+    q = true,
+    S = true,
+  })
+
+  vim.opt.report = 9999
+end
